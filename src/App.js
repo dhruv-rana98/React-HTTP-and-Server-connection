5e3de8d3234cbb51aeb2204af78e981b65dc578f
Dhruv Rana <dhruvrana0000@gmail.com>
2022-06-17 16:05:22 +0530

    Error handling by using the response object ok field or status filed and then throw an error from the try block and use the catch block to catch the error and set the error state and finally setLoading state to false to stop loading as error was encountered

--- src/App.js
+++ src/App.js
@@ -6,21 +6,30 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]); //movie getting state
   const [isLoading, setIsLoading] = useState(false); //Loading state
+  const [error, setError] = useState(null); //Error state to handle error response from server such as 401, 403
 
   async function fecthMoviesHandler() {
     setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
-    //Transforming the movie data to an object using map function to access values within the Item of results array which is being passed as props to the list of movie
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+    setError(null); //clear previos errors from the previous request
+    try {
+      const response = await fetch("https://swapi.dev/api/film/");
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const data = await response.json();
+      //Transforming the movie data to an object using map function to access values within the Item of results array which is being passed as props to the list of movie
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
   }
 
@@ -31,8 +40,9 @@ function App() {
       </section>
       <section>
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no Movies.</p>}
+        {!isLoading && movies.length === 0 && !error && <p>Found no Movies.</p>}
         {isLoading && <p>Loading... Please wait!</p>}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
